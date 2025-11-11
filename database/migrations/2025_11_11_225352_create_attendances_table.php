b2d4a2e8e2216320f34d63a7d82eb7333f7114a0
JormanOT <jormanortega34@gmail.com>
2025-11-11 19:01:14 -0400

    [FEATURE] Se creo el modelo y la migracion para el modulo de asistencias

---
+++ database/migrations/2025_11_11_225352_create_attendances_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained()->onDelete('cascade');      
+            $table->dateTime('check_in_at');
+            $table->dateTime('check_out_at')->nullable(); 
+            $table->string('check_in_ip')->nullable();
+            $table->string('check_out_ip')->nullable();
+            $table->enum('status', ['normal', 'tardy', 'corrected'])->default('normal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
